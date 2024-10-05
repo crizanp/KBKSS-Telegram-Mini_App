6b316fde91465a59648d104c4a813ebad31c3a5c
srijan703 <srijanpokhrel343@gmail.com>
2024-10-05 11:24:02 +0545

    updatded avatar selection

--- src/utils/getUserID.js
+++ src/utils/getUserID.js
@@ -19,7 +19,7 @@ export const getUserID = async (setUserID = () => {}, setUsername = () => {}) =>
 
   // For localhost testing
   if (isLocalhost) {
-    tgUserID = "123";
+    tgUserID = "mockUserID1223";
     tgUsername = "mockUsername"; // Mock username for testing
     tgFirstName = "MockFirstName"; // Mock first name for testing
     console.warn(
