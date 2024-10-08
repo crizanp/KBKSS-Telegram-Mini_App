af5c491ee229a3444dfec2e89ae9f648ed1cc360
srijan703 <srijanpokhrel343@gmail.com>
2024-10-08 12:15:08 +0545

    weekly leaderboard enhancement

--- src/pages/LeaderboardPage.js
+++ src/pages/LeaderboardPage.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import styled, { css } from "styled-components";
 import { FaGamepad, FaTasks, FaUserFriends, FaRegGem } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import SkeletonLoader from "../components/skeleton/SkeletonLoader"; // Keep using your SkeletonLoader component
+import SkeletonLoader from "../components/skeleton/SkeletonLoader";
 import {
   EarnBoxTitle,
   EarnBoxIcon,
@@ -25,41 +25,61 @@ import {
   avatarImages,
 } from "../style/LeaderboardStyles";
 
-// Fetching the top users data
-const fetchTopUsers = async () => {
-  const response = await axios.get(`${process.env.REACT_APP_API_URL}/user-info/fetchdata`);
-  return response.data.sort((a, b) => b.points - a.points).slice(0, 30); // Sort and limit to 30 users
-};
+// Styled component for Tabs container
+const TabContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
+`;
 
-// Styled component for announcement box with improved spacing and hover effect
-export const AnnouncementBox = styled(Link)`
-  background-color: #6023f5;
-  color: #ffffff;
-  padding: 14px;
-  text-align: center;
-  margin: 20px auto;
-  display: block;
-  font-size: 20px;
+// Styled component for each Tab
+const Tab = styled.button`
+  padding: 10px 20px;
+  font-size: 18px;
   font-weight: bold;
-  width: 90%; /* Ensures it scales well on smaller screens */
+  background-color: ${(props) => (props.active ? "#6023f5" : "#f1f1f1")};
+  color: ${(props) => (props.active ? "#ffffff" : "#000000")};
+  border: none;
   border-radius: 8px;
-  text-decoration: none;
-  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  margin: 0 10px;
+  transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: #4c1bb5;
-    transform: translateY(-3px);
+    background-color: ${(props) => (props.active ? "#4c1bb5" : "#d9d9d9")};
   }
 `;
 
+// Fetching the top users data for Lifetime leaderboard
+const fetchTopUsers = async () => {
+  const response = await axios.get(`${process.env.REACT_APP_API_URL}/user-info/fetchdata`);
+  return response.data.sort((a, b) => b.points - a.points).slice(0, 30);
+};
+
+// Fetching the top users data for Weekly leaderboard
+const fetchWeeklyTopUsers = async () => {
+  const response = await axios.get(`${process.env.REACT_APP_API_URL}/user-info/leaderboard/weekly`);
+  return response.data.filter((user) => user.weeklyPoints > 0).slice(0, 10); // Filter out users with 0 points
+};
+
 function LeaderboardPage() {
-  const { data: users = [], isLoading, isError } = useQuery(
-    ["topUsers"],
-    fetchTopUsers,
+  const [activeTab, setActiveTab] = useState("lifetime");
+
+  // Fetch Lifetime leaderboard data
+  const { data: users = [], isLoading, isError } = useQuery(["topUsers"], fetchTopUsers, {
+    staleTime: 1000 * 60 * 5,
+    cacheTime: 1000 * 60 * 30,
+    refetchOnWindowFocus: false,
+  });
+
+  // Fetch Weekly leaderboard data
+  const { data: weeklyUsers = [], isLoading: isWeeklyLoading, isError: isWeeklyError } = useQuery(
+    ["weeklyTopUsers"],
+    fetchWeeklyTopUsers,
     {
-      staleTime: 1000 * 60 * 5, // Data remains fresh for 5 minutes
-      cacheTime: 1000 * 60 * 30, // Cache data for 30 minutes
-      refetchOnWindowFocus: false, // Prevent automatic refetch on window focus
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 30,
+      refetchOnWindowFocus: false,
     }
   );
 
@@ -80,42 +100,63 @@ function LeaderboardPage() {
     }
   };
 
-  if (isError) {
-    return <NoUsersMessage>Error fetching leaderboard data. Please try again later.</NoUsersMessage>;
-  }
+  // Handle tab switching
+  const renderLeaderboard = () => {
+    if (isLoading || isWeeklyLoading) {
+      return <SkeletonLoader />;
+    }
+
+    if ((activeTab === "lifetime" && users.length === 0) || (activeTab === "weekly" && weeklyUsers.length === 0)) {
+      return <NoUsersMessage>No users found</NoUsersMessage>;
+    }
+
+    const leaderboardData = activeTab === "lifetime" ? users : weeklyUsers;
+    return (
+      <Table>
+        <thead>
+          <tr>
+            <TableHeader>#</TableHeader>
+            <TableHeader>User</TableHeader>
+            <TableHeader>{activeTab === "lifetime" ? "Total" : "Weekly"} $GEMS</TableHeader>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboardData.map((user, index) => (
+            <TableRow key={user.userID}>
+              <RankCell rank={index + 1}>
+                {index + 1 === 1 && <img src="https://i.ibb.co/5cZBk7J/3d-5.png" alt="First Place" />}
+                {index + 1 === 2 && <img src="https://i.ibb.co/swJQnL0/3d-6.png" alt="Second Place" />}
+                {index + 1 === 3 && <img src="https://i.ibb.co/tqBDBFv/3d-7.png" alt="Third Place" />}
+                {index + 1 > 3 && index + 1}
+              </RankCell>
+              <UserCell>
+                <UserAvatar src={avatarImages[(user.userID % 20) + 1]} alt="User Avatar" />
+                <span>{truncateUsername(user.username)}</span>
+              </UserCell>
+              <PointsCell>
+                <FaRegGem style={{ marginRight: "8px", color: "#36a8e5" }} />
+                {formatPoints(activeTab === "lifetime" ? user.points : user.weeklyPoints)}
+              </PointsCell>
+            </TableRow>
+          ))}
+        </tbody>
+      </Table>
+    );
+  };
 
   return (
     <>
-      {/* Announcement Box */}
-      <AnnouncementBox to="/weekly-leaderboard">
-        🎉 Weekly leaderboard is open! Click here 🎉
-      </AnnouncementBox>
+      {/* Tab Navigation */}
+      <TabContainer>
+        <Tab active={activeTab === "lifetime"} onClick={() => setActiveTab("lifetime")}>
+          Lifetime Leaderboard
+        </Tab>
+        <Tab active={activeTab === "weekly"} onClick={() => setActiveTab("weekly")}>
+          Weekly Leaderboard
+        </Tab>
+      </TabContainer>
 
-      {/* Earn More Container */}
-      <EarnMoreContainer>
-        <EarnBox to="/home">
-          <EarnBoxIcon>
-            <FaGamepad />
-          </EarnBoxIcon>
-          <EarnBoxTitle>Play Game</EarnBoxTitle>
-        </EarnBox>
-
-        <EarnBox to="/earn">
-          <EarnBoxIcon>
-            <FaTasks />
-          </EarnBoxIcon>
-          <EarnBoxTitle>Complete Task</EarnBoxTitle>
-        </EarnBox>
-
-        <EarnBox to="/friend">
-          <EarnBoxIcon>
-            <FaUserFriends />
-          </EarnBoxIcon>
-          <EarnBoxTitle>Refer Friends</EarnBoxTitle>
-        </EarnBox>
-      </EarnMoreContainer>
-
-      {/* Leaderboard Section */}
+      {/* Leaderboard Content */}
       <LeaderboardContainer>
         <PointsDisplayContainer>
           <PointsDisplay>
@@ -125,50 +166,12 @@ function LeaderboardPage() {
               style={{ width: "150px", height: "150px", marginTop: "20px" }}
             />
           </PointsDisplay>
-          <Top30LeaderText>Top 30 Leaders</Top30LeaderText>
+          <Top30LeaderText>Top {activeTab === "lifetime" ? "30 Lifetime" : "10 Weekly"} Leaders</Top30LeaderText>
         </PointsDisplayContainer>
-
-        {/* Display Skeleton Loader when loading */}
-        {isLoading ? (
-          <SkeletonLoader />
-        ) : users.length === 0 ? (
-          <NoUsersMessage>No users found</NoUsersMessage>
+        {isError || isWeeklyError ? (
+          <NoUsersMessage>Error fetching leaderboard data. Please try again later.</NoUsersMessage>
         ) : (
-          <Table>
-            <thead>
-              <tr>
-                <TableHeader>#</TableHeader>
-                <TableHeader>User</TableHeader>
-                <TableHeader>Total</TableHeader>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user, index) => (
-                <TableRow key={user.userID}>
-                  <RankCell rank={index + 1}>
-                    {index + 1 === 1 && (
-                      <img src="https://i.ibb.co/5cZBk7J/3d-5.png" alt="First Place" />
-                    )}
-                    {index + 1 === 2 && (
-                      <img src="https://i.ibb.co/swJQnL0/3d-6.png" alt="Second Place" />
-                    )}
-                    {index + 1 === 3 && (
-                      <img src="https://i.ibb.co/tqBDBFv/3d-7.png" alt="Third Place" />
-                    )}
-                    {index + 1 > 3 && index + 1}
-                  </RankCell>
-                  <UserCell>
-                    <UserAvatar src={avatarImages[(user.userID % 20) + 1]} alt="User Avatar" />
-                    <span>{truncateUsername(user.username)}</span>
-                  </UserCell>
-                  <PointsCell>
-                    <FaRegGem style={{ marginRight: "8px", color: "#36a8e5" }} />
-                    {formatPoints(user.points)}
-                  </PointsCell>
-                </TableRow>
-              ))}
-            </tbody>
-          </Table>
+          renderLeaderboard()
         )}
       </LeaderboardContainer>
     </>
